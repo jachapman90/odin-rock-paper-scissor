8157fef25cc06fc7f757f4734f8f816639feda6b
jachapman90 <jachapman.1990@gmail.com>
2024-01-11 18:12:49 -0500

    Add RPC functionality

--- script.js
+++ script.js
@@ -1,14 +1,87 @@
-const choice = getComputerChoice;
+let winCount = 0;
+let lossCount = 0;
+
+let playerChoice = "";
+let computerChoice = "";
+
+function playGame() {
+playerChoice = getPlayerChoice();
+computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
+playerChoice = getPlayerChoice();
+computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
+playerChoice = getPlayerChoice();
+computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
+playerChoice = getPlayerChoice();
+computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
+playerChoice = getPlayerChoice();
+computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
+}
+
+function getPlayerChoice() {
+    let playerInput = prompt("Rock, Paper, Scissors?");
+    return(playerInput.toLowerCase());
+}
+
+function playRound(p1, p2) {
+    if (playerChoice === computerChoice) {
+        console.log(p1);
+        console.log(p2);
+        console.log("Tie, Play Again!");
+    } else if ((playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "rock")) {
+        console.log(p1);
+        console.log(p2);
+        lossCount++;
+        console.log("You Lose!");
+    } else {
+        console.log(p1);
+        console.log(p2);
+        winCount++;
+        console.log("You Win!");
+    }
+
+    console.log("You: " + winCount);
+    console.log("Computer: " + lossCount);
+
+    if (winCount >= 3) {
+        winCount = 0;
+        lossCount = 0;
+        return(alert("You Win The Game!"));
+    } else if (lossCount >= 3) {
+        winCount = 0;
+        lossCount = 0;
+        return(alert("You Lose The Game!"));
+    }
+  }
+
+
 
 function getComputerChoice() {
-    let computerChoice = (Math.floor(Math.random() * 10)) % 3;
-    if (computerChoice == 0) {
-        computerChoice = "Rock";
+    let outputChoice;
+    let computerInput = (Math.floor(Math.random() * 10)) % 3;
+    if (computerInput === 0) {
+        outputChoice = "rock";
     }
-    else if (computerChoice == 1) {
-        computerChoice = "Paper";
+    else if (computerInput === 1) {
+        outputChoice = "paper";
     }
-    else {computerChoice = "Scissors";
+    else {outputChoice = "scissors";
     }
-    return computerChoice;
-}
+    return(outputChoice);
+} 
+
+
+
+
+
