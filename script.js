4d30222bc1ad55930b36f6cc46f6417a1f0dbaa8
jachapman90 <jachapman.1990@gmail.com>
2024-02-02 13:37:03 -0500

    Add GUI, tweak scoring, and remove unneeded code

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const div = document.querySelector('#rps');
 const score = document.querySelector('#score');
 const losses = document.querySelector('#losses');
 const wins = document.querySelector('#wins');
-
+const round = document.querySelector('#round');
+const playerHand= document.querySelector('#playerHand');
+const compHand = document.querySelector('#compHand');
 
 let winCount = 0;
 let lossCount = 0;
 let gameCount = 0;
 let winner;
-
 let playerChoice = "";
 let computerChoice = "";
 
@@ -36,22 +37,30 @@ div.addEventListener('click', (event) => {
     };
 });
 
-function playGame() {
-    while (gameCount < 5) {
-//    playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-    scoreCount();
+function scoreCount() {
+    if (winner === "comp") {
+        lossCount++;
+        gameCount++;
+        losses.textContent = `Losses: ${lossCount}`;
+        round.textContent = `Round: ${gameCount}`;
+        winner = "";
+    } else if (winner === "player") {
+        winCount++;
+        gameCount++;
+        wins.textContent = `Wins: ${winCount}`;
+        round.textContent = `Round: ${gameCount}`;
+        winner = "";
     };
-    console.log("You: " + winCount);
-    console.log("Computer: " + lossCount);
-    if (gameCount === 5 && winCount > lossCount) {
+};
+
+function winCon() {
+    if (winCount === 5) {
         (alert("You won" + "\n" + winCount + "/5 rounds." + "\n" + 
         "You Win The Game!"));
         winCount = 0;
         lossCount = 0;
         gameCount = 0;
-    } else if (gameCount === 5 && winCount < lossCount) {
+    } else if (lossCount === 5) {
         (alert("You won" + "\n" + winCount + "/5 rounds." + "\n" + 
         "You Lost The Game!"));
         winCount = 0;
@@ -60,53 +69,27 @@ function playGame() {
     };
 };
 
-function scoreCount() {
-    if (winner === "comp") {
-        lossCount++;
-        gameCount++;
-        losses.textContent = `Losses: ${lossCount}`;
-        winner = "";
-        console.log(gameCount);
-    } else if (winner === "player") {
-        winCount++;
-        gameCount++;
-        wins.textContent = `Wins: ${winCount}`;
-        winner = "";
-        console.log(gameCount);
-    };
-};
-
-function getPlayerChoice() {
-    let playerInput = prompt("Rock, Paper, Scissors?");
-    return(playerInput.toLowerCase());
-};
-
 function playRound(p1, p2) {
     if (playerChoice === computerChoice) {
-        console.log(p1);
-        console.log(p2);
         console.log("Tie, Play Again!");
     } else if ((playerChoice === "rock" && computerChoice === "paper") ||
         (playerChoice === "paper" && computerChoice === "scissors") ||
         (playerChoice === "scissors" && computerChoice === "rock")) {
-        console.log(p1);
-        console.log(p2);
         winner = "comp";
         console.log("You Lose!");
-        scoreCount();
-        return(winner);
     } else {
-        console.log(p1);
-        console.log(p2);
         winner = "player";
         console.log("You Win!");
-        scoreCount();
-        return(winner);
     };
-
+    showHands()
+    scoreCount();
+    winCon();
 };
 
-
+function showHands() {
+    playerHand.textContent = `You played: ${playerChoice}`;
+    compHand.textContent = `Computer played: ${computerChoice}`;
+};
 
 function getComputerChoice() {
     let outputChoice;
@@ -120,7 +103,6 @@ function getComputerChoice() {
     return(outputChoice);
 };
 
-//playGame();
 
 
 
